e420546ece65b9101c751013a81444ed38c417cf
Maria Duarte <mariaduarte061@hotmail.com>
2026-01-08 12:00:20 -0300

    feat: add mobile navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,23 +1,102 @@
-import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+import * as React from 'react';
+import { AppBar, IconButton, Menu, MenuItem, styled, Toolbar } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import { Box } from '@mui/system';
 
-const NavBar = () => {
+export const StyledNavLink = styled("a")(() => ({
+    textDecoration: "none",
+    color: "inherit"
+}));
 
-    const StyledToobar = styled(Toolbar)(() => ({
+export const StyledMobileToolbar = styled(Toolbar)(({ theme }) => ({
+    [theme.breakpoints.up('xs')]: {
+        display: "flex",
+        justifyContent: "end",
+    },
+    [theme.breakpoints.up('md')]: {
+        display: "none",
+    }
+}));
+
+export const StyledDesktopToolbar = styled(Toolbar)(({ theme }) => ({
+    [theme.breakpoints.up('xs')]: {
+        display: "none",
+    },
+    [theme.breakpoints.up('md')]: {
         display: "flex",
         justifyContent: "space-evenly",
-    }))
+    }
+}));
+
+const NavBar = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleSmoothScroll = (id: string) => {
+        const element = document.getElementById(id);
+        if(element){
+            element.scrollIntoView({behavior: "smooth"});
+            handleClose();
+        }
+    }
 
     return (
-        <>
+        <Box>
             <AppBar position="absolute">
-                <StyledToobar>
-                    <MenuItem>About</MenuItem>
-                    <MenuItem>Skills</MenuItem>
-                    <MenuItem>Project</MenuItem>
-                </StyledToobar>
+                <StyledMobileToolbar>
+                    <IconButton 
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        color="inherit"
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Menu id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={() => handleSmoothScroll("about")}>
+                            <StyledNavLink>About</StyledNavLink>
+                        </MenuItem>
+                        <MenuItem onClick={() => handleSmoothScroll("skills")}>
+                            <StyledNavLink>Skills</StyledNavLink>
+                        </MenuItem>
+                        <MenuItem onClick={() => handleSmoothScroll("projects")}>
+                            <StyledNavLink>Projects</StyledNavLink>
+                        </MenuItem>
+                    </Menu>
+                </StyledMobileToolbar>
+
+                <StyledDesktopToolbar variant="regular">
+                    <MenuItem onClick={() => handleSmoothScroll("about")}>
+                        <StyledNavLink>About</StyledNavLink>
+                    </MenuItem>
+                    <MenuItem onClick={() => handleSmoothScroll("skills")}>
+                        <StyledNavLink>Skills</StyledNavLink>
+                    </MenuItem>
+                    <MenuItem onClick={() => handleSmoothScroll("projects")}>
+                        <StyledNavLink>Projects</StyledNavLink>
+                    </MenuItem>
+                </StyledDesktopToolbar>
             </AppBar>
-        </>
+        </Box>
     )
 }
 
-export default NavBar
+export default NavBar;
