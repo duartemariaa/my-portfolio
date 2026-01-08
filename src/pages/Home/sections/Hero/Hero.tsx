d39f41992d2bd0c965ba33050bb348617edeb16d
Maria Duarte <mariaduarte061@hotmail.com>
2026-01-08 12:03:55 -0300

    feat: add cv and contact

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -2,30 +2,55 @@ import {styled, Container, Typography, Box} from '@mui/material'
 import Grid from '@mui/material/Grid'
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import Avatar from '../../../../assets/images/avatar.jpeg'
+import Avatar from '../../../../assets/images/avatar.jpeg';
 import StyledButton from '../../../../components/StyledButton/StyledButton.tsx';
 import { AnimatedBackground } from '../../../../components/AnimatedBackground/AnimatedBackground.tsx';
+import CV from "../../../../assets/pdfs/mariajduartebarbosaa.pdf";
 
 const Hero = () => {
 
     const StyledHero = styled('div')(({theme})=> ({
         backgroundColor: theme.palette.primary.main,
-        height: '100vh',
-        display: "flex",
-        alignItems: "center",
+        width: "100%",
         [theme.breakpoints.up('xs')]: {
+            display: "block",
+            padding: "20px",
             paddingTop: "100px",
+            paddingBottom: "40px",
         },
         [theme.breakpoints.up('md')]: {
-            paddingTop: "0",
-        }
-    }))
+            display: "flex",
+            alignItems: "center",
+            paddingTop: "100px",
+            height: "100vh"
+        },
+    }));
 
     const StyledImg = styled('img')(({theme})=> ({
         width: '75%',
         borderRadius: '50%',
         border: `1px solid ${theme.palette.primary.contrastText}`,
-    }))
+        position: "relative"
+    }));
+
+    const handleDownload = () => {
+        console.log("download")
+        const link = document.createElement('a');
+        link.href = CV
+        link.download = 'example.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    const handleEmail = () => {
+        const emailAddress = 'example@example.com';
+        const subject = 'Subject';
+        const body = 'Hello! I saw your portfolio...';
+
+        const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+        window.open(mailtoLink);
+    };
 
     return (
         <>
@@ -33,36 +58,39 @@ const Hero = () => {
                 <Container maxWidth="lg">
                     <Grid container spacing={2}>
                         <Grid item xs={12} md={5}>
-                            <Box position="relative">
+                            <Box position="relative" pb={3}>
                                 <Box position="absolute" width={"150%"} top={-100} right={0}>
                                     <AnimatedBackground/>
                                 </Box>
-                                <Box position="relative" textAlign="center">
+                                <Box textAlign="center">
                                     <StyledImg src={Avatar}/>
                                 </Box>
                             </Box>
                         </Grid>
+
+
                         <Grid item xs={12} md={7}>
                             <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Maria Duarte</Typography>
                             <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Enginner</Typography>
-                            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
-                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                    <StyledButton onClick={() => console.log("download")}>
-                                        <DownloadIcon/>
-                                        <Typography>Download CV</Typography>
-                                    </StyledButton>
+                            <Box mt={3}>
+                                <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+                                    <Grid item xs={12} md={4}>
+                                        <StyledButton onClick={() => handleDownload()}>
+                                            <DownloadIcon/>
+                                            <Typography>Download CV</Typography>
+                                        </StyledButton>
+                                    </Grid>
+                                    <Grid item xs={12} md={4}>
+                                        <StyledButton onClick={() => handleEmail()}>
+                                            <MailOutlineIcon/>
+                                            <Typography>Contact me</Typography>
+                                        </StyledButton>
+                                    </Grid>
                                 </Grid>
-                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                    <StyledButton onClick={() => console.log("contact")}>
-                                        <MailOutlineIcon/>
-                                        <Typography>Contact me</Typography>
-                                    </StyledButton>
-                                </Grid>
-                            </Grid>
+                            </Box>
                         </Grid>
                     </Grid>
                 </Container>
-
             </StyledHero>
 
         </>
--- src/theme.ts
+++ src/theme.ts
@@ -11,7 +11,7 @@ let theme = createTheme({
     },
 
     typography: {
-        fontFamily: "Helvetica Neue"
+      fontFamily: "Helvetica Neue"
     }
 });
 
